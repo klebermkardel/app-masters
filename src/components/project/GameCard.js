306ecfe3d272b6f3652649b2cfb506699a08ad67
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-23 21:07:04 -0300

    Update GameCard.js

--- src/components/project/GameCard.js
+++ src/components/project/GameCard.js
@@ -7,10 +7,10 @@ const GameCard = ({ id, title, genre, developer, release_date, thumbnail }) => {
   return (
     <div key={id} className="game-card">
       <img src={thumbnail} alt={title} className="game-card-image" />
-      <h3 className="game-card-title">Título: {title}</h3>
+      <h3 className="game-card-title">{title}</h3>
       <p className="game-card-p">Gênero: {genre}</p>
       <p className="game-card-p">Desenvolvedora: {developer}</p>
-      <p className="game-card-p">Desenvolvedora: {release_date}</p>
+      <p className="game-card-p">Lançamento: {release_date}</p>
     </div>
   );
 };
